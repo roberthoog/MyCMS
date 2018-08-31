cfc45ec271e13ee0a346a42422c8edc48e7f9e84
roberthoog <minadress@gmail.com>
2018-08-31 16:54:57 +0200

    first commit

---
+++ inc/db.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$databaseHost = 'localhost';
+$databaseName = 'cms';
+$databaseUsername = 'root';
+$databasePassword = 'r00t';
+define( 'DB_CHARSET', 'utf8');
+ini_set('default_charset', 'utf-8');
+
+try {
+    // http://php.net/manual/en/pdo.connections.php
+    $dbConn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
+
+    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
+    // More on setAttribute: http://php.net/manual/en/pdo.setattribute.php
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+if($dbConn) {
+    echo 'YAAAAAAAAAAAAAAY';
+    }
+?>
